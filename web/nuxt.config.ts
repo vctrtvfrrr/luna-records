704c450a6e87e566c8155e0f1fa97d1bba3f0a04
Victor Otávio Ferreira <victor@otavioferreira.com.br>
2023-01-25 10:33:18 -0300

    chore(web): Setup TailwindCSS

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -1,11 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  css: ['~/assets/css/main.css'],
   devServer: {
     host: process.env.HOST,
     port: Number(process.env.PORT),
   },
   typescript: {
     strict: true,
+  },
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
   },
   // TODO: Nmbiente de desenvolvimento, as chamadas HMR precisam passar pelo proxy reverso do NGINX.
   vite: {
