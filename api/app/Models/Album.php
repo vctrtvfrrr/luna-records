195c5684abe30d514f9215013e41232257c4a58e
Victor Otávio Ferreira <victor@otavioferreira.com.br>
2023-01-27 10:21:12 -0300

    models

---
+++ api/app/Models/Album.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Album extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'artist',
+        'cover',
+        'released_at',
+        'duration',
+        'stock',
+        'price',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'released_at' => 'datetime',
+        'duration'    => 'integer',
+        'stock'       => 'integer',
+        'price'       => 'integer',
+    ];
+
+    /**
+     * Get the tracks for the album.
+     */
+    public function tracks(): HasMany
+    {
+        return $this->hasMany(Track::class);
+    }
+
+    /**
+     * The tags that belong to the album.
+     */
+    public function tags(): BelongsToMany
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+}
---
+++ api/app/Models/Tag.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Tag extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+    ];
+
+    /**
+     * The albums that belong to the tag.
+     */
+    public function albums(): BelongsToMany
+    {
+        return $this->belongsToMany(Album::class);
+    }
+}
